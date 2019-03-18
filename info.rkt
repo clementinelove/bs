e1334e0c577e7ed17b40e938db6781884302ee4f
oldsin <oldsin@live.com>
2019-03-18 00:26:45 +0000

    fix dependency problem

--- info.rkt
+++ info.rkt
@@ -3,10 +3,10 @@
 (define version "1.0")
 
 (define scribblings '(("scribblings/bs.scrbl")))
+
 (define deps '("base"
                "brag"
                "crypto-lib"
                "parser-tools-lib"))
-(define build-deps '("racket-doc"
-                     "rackunit-lib"
+(define build-deps '("rackunit-lib"
                      "scribble-lib"))
